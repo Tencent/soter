fa5cad7c64c50bf6663d2ddbf451a76bd1ff2998
yexuan <yexuan910812@gmail.com>
2017-08-02 19:50:21 +0800

    [soter] add netwrapper picture indicator

---
+++ soter-client-demo/app/src/main/java/com/tencent/soter/demo/ui/SoterSampleUI.java
@@ -0,0 +1,10 @@
+package com.tencent.soter.demo.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by henryye on 2017/8/2.
+ */
+
+public class SoterSampleUI extends AppCompatActivity {
+}
