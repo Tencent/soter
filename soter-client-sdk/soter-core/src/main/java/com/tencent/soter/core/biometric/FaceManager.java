f3ed8454ba85b0cfa9a04e335a8f1c4b9b24a1b7
peteryan <peteryan@tencent.com>
2018-07-05 11:04:43 +0800

    (refactor) upgrade sdk to 1.3.40 for demo test FaceManager add comment for OEM developer to support

--- soter-client-sdk/soter-core/src/main/java/com/tencent/soter/core/biometric/FaceManager.java
+++ soter-client-sdk/soter-core/src/main/java/com/tencent/soter/core/biometric/FaceManager.java
@@ -18,49 +18,152 @@ import javax.crypto.Mac;
 public abstract class FaceManager {
     private static final String TAG = "Soter.FaceManager";
 
-    /**Error state returned when the current request has been running too long.*/
-    public static final int FACE_ERROR_TIMEOUT = 3;
+    /**The hardware is unavailable. Try again later.*/
+    public static final int FACE_ERROR_HW_UNAVAILABLE = 1;
 
-    /**The operation was canceled. SOTER treat it as onAuthenticationCanceled*/
-    public static final int FACE_ERROR_CANCELED = 5;
+    /**Error state returned when the sensor was unable to process the current image.*/
+    public static final int  FACE_ERROR_UNABLE_TO_PROCESS = 2;
+
+    /**Error state returned when the current request has been running too long.*/
+    public static final int  FACE_ERROR_TIMEOUT = 3;
+
+    /**
+     * The operation was canceled because the sensor is unavailable. For example,
+     * this may happen when the user is switched, the device is locked or another pending operation
+     * prevents or disables it.
+     */
+    public static final int  FACE_ERROR_CANCELED = 5;
 
     /**The operation was canceled because the API is locked out due to too many attempts.*/
-    public static final int FACE_ERROR_LOCKOUT = 7;
+    public static final int  FACE_ERROR_LOCKOUT = 7;
 
+    /**Error state returned when the camera was unable to process the current image.*/
+    public static final int  FACE_ERROR_CAMERA_UNAVAILABLE = 8;
 
+    /**
+     * Hardware vendors may extend this list if there are conditions that do not fall under one of
+     * the above categories. Vendors are responsible for providing error strings for these errors.
+     */
+    public static final int  FACE_ERROR_VENDOR_BASE = 1000;
 
-    public static final int FACE_ERROR_VENDOR_BASE = 1000;
-    /**The image acquired was good.*/
+    //The following error statuses will be notified to onAuthenticationHelp, they are recoverable error.
+    /**
+     * The image acquired was good.
+     */
     public static final int FACE_ACQUIRED_GOOD = 1101;
-    /**The face image was too noisy to process due to a detected condition (i.e. dry skin) or a possibly dirty sensor.*/
+
+    /**
+     * The face image was too noisy to process due to a detected condition (i.e. dry skin) or
+     * a possibly dirty sensor (See {@link #FACE_ACQUIRED_IMAGER_DIRTY}).
+     */
     public static final int FACE_ACQUIRED_INSUFFICIENT = 1102;
-    /**The face image was too noisy due to suspected or detected dirt on the sensor. */
+
+    /**
+     * The face image was too noisy due to suspected or detected dirt on the sensor.
+     * For example, it's reasonable return this after multiple
+     * {@link #FACE_ACQUIRED_INSUFFICIENT} or actual detection of dirt on the sensor
+     * (stuck pixels, swaths, etc.). The user is expected to take action to clean the sensor
+     * when this is returned.
+     */
     public static final int FACE_ACQUIRED_IMAGER_DIRTY = 1103;
+
+    /**
+     * The face image was unreadable due to lack of motion. This is most appropriate for
+     * linear array sensors that require a swipe motion.
+     */
     public static final int FACE_ACQUIRED_TOO_SLOW = 1104;
+
+    /**
+     * The face image was incomplete due to quick motion. While mostly appropriate for
+     * linear array sensors,  this could also happen if the face was moved during acquisition.
+     * The user should be asked to move the face slower (linear) or leave the face on the sensor
+     * longer.
+     */
     public static final int FACE_ACQUIRED_TOO_FAST = 1105;
-    /**The face image was too close to the phone screen, need to take it off the screen to get a bigger face rect */
+
+    /**
+     * The face image was too close to the phone screen, need to take it off the screen to get a
+     * bigger face rect
+     */
     public static final int FACE_ACQUIRED_FAR_FACE = 1106;
-    /**The face image was too far from the phone screen, need to take it more close to the screen to get a bigger face rect*/
+
+    /**
+     * The face image was too far from the phone screen, need to take it more close to the screen
+     * to get a bigger face rect
+     */
     public static final int FACE_ACQUIRED_NEAR_FACE = 1107;
+
+    /**
+     * There is no face in the image
+     */
     public static final int FACE_ACQUIRED_NO_FACE = 1108;
+
+    /**
+     * Face needs to shift to make a whole face scan. (not use in 3D)
+     */
     public static final int FACE_ACQUIRED_SHIFTING = 1109;
+
+    /**
+     * The light is too bright, needs darker
+     */
     public static final int FACE_ACQUIRED_DARK = 1110;
+
+    /**
+     * The image is regarded as a hacker attack
+     */
     public static final int FACE_ACQUIRED_HACKER = 1111;
+
+    /**
+     * The light is too dim, needs brighter
+     */
     public static final int FACE_ACQUIRED_BRIGHT = 1112;
+
+    //for face angle
+    /**
+     * Should turn the face to left
+     */
     public static final int FACE_ACQUIRED_LEFT = 1113;
+
+    /**
+     * Should turn the face to right
+     */
     public static final int FACE_ACQUIRED_RIGHT = 1114;
+
+    /**
+     * Should lift the face
+     */
     public static final int FACE_ACQUIRED_UP = 1115;
+
+    /**
+     * Should lower the face
+     */
     public static final int FACE_ACQUIRED_DOWN = 1116;
+
+    /**
+     * Eyes are closed in the image.
+     */
     public static final int FACE_WITH_EYES_CLOSED = 1117;
+
+    /**
+     * The face has no focus on the phone.
+     */
     public static final int FACE_ACQUIRED_NO_FOCUS = 1118;
+
+    /**
+     * The mouth is covered
+     */
     public static final int FACE_ACQUIRED_MOUTH_OCCLUSION = 1119;
-    /**used in enroll, if phone is rotate and enroll may cause this message.*/
-    public static final int FACE_ACQUIRED_NOT_FRONTAL_FACE = 1120;
-    public static final int FACE_ACQUIRED_NOSE_OCCLUSION = 1121;
-    /**nose occlusion.*/
-    public static final int FACE_ACQUIRED_MULTI_FACE = 1122;
-    public static final int FACE_ERROR_CAMERA_UNAVAILABLE = 1001;
-    public static final int FACE_ERROR_HW_UNAVAILABLE = 1002;
+
+    /**
+     * The nose is covered
+     */
+    public static final int FACE_ACQUIRED_NOSE_OCCLUSION = 1120; /* nose occlusion. */
+
+    /**
+     * There are more than one face in the image
+     */
+    public static final int FACE_ACQUIRED_MULTI_FACE = 1121;
+    ///
 
     public static final class CryptoObject {
 
