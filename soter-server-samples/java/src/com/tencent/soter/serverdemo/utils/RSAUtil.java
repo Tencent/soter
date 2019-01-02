453f04a0bdf7fa94ac2901e3cc2a4e03a126e8a8
tofuliu <tofuliu@tencent.com>
2019-01-02 20:59:46 +0800

    [refine]: update server-samples

--- soter-server-samples/java/src/com/tencent/soter/serverdemo/utils/RSAUtil.java
+++ soter-server-samples/java/src/com/tencent/soter/serverdemo/utils/RSAUtil.java
@@ -1,19 +1,23 @@
 package com.tencent.soter.serverdemo.utils;
 
+import java.io.IOException;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.cert.X509Certificate;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.MGF1ParameterSpec;
 import java.security.spec.PSSParameterSpec;
 import java.security.spec.X509EncodedKeySpec;
 
+import com.tencent.soter.serverdemo.SoterPubKeyModel;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Base64;
+import org.json.JSONObject;
 
 /**
  * RSAUtil is the helper class to load keys and verify signature and so on.
@@ -22,7 +26,9 @@ import org.bouncycastle.util.encoders.Base64;
 public class RSAUtil {
 
 	private static final String ALGORITHM_NAME = "RSA";
-	
+
+	private static final String KEY_DESCRIPTION_OID = "1.3.6.1.4.1.11129.2.1.17";
+
 	/**
 	 * load PublicKey from file
 	 */
@@ -79,5 +85,49 @@ public class RSAUtil {
 
 	public static boolean verify(RSAPublicKey publicKey, String data, byte[] signature) {
 		return verify(publicKey, data.getBytes(), signature);
+	}
+
+	public static void extractAttestationSequence(X509Certificate attestationCert, SoterPubKeyModel soterPubKeyModel) throws Exception, IOException {
+		byte[] attestationExtensionBytes = attestationCert.getExtensionValue(KEY_DESCRIPTION_OID);
+		if (attestationExtensionBytes == null || attestationExtensionBytes.length == 0) {
+			throw new Exception("Couldn't find the keystore attestation " + "extension data.");
+		}
+
+		try {
+			int jsonStartOff = 0;
+			int jsonEndOff = 0;
+			int jsonLength = 0;
+
+			byte jsonStartTag = "{".getBytes()[0];
+			byte jsonEndTag = "}".getBytes()[0];
+
+			for (int i = 0; i < attestationExtensionBytes.length; i++) {
+				byte b = attestationExtensionBytes[i];
+				if (b==jsonStartTag) {
+					jsonStartOff = i;
+				}else if(b==jsonEndTag){
+					jsonEndOff = i;
+				}
+			}
+			if (jsonStartOff > 0 && jsonStartOff < jsonEndOff) {
+				assert attestationExtensionBytes[jsonStartOff-1]==(jsonEndOff-jsonStartOff+1);
+				jsonLength = (jsonEndOff-jsonStartOff+1);
+
+				byte[] jsonBytes = new byte[jsonLength];
+				System.arraycopy(attestationExtensionBytes, jsonStartOff, jsonBytes, 0, jsonLength);
+				String jsonString = new String(jsonBytes);
+				JSONObject jsonObject = new JSONObject(jsonString);
+
+				System.out.println("soter: challenge json in attestation certificate " + jsonString);
+
+
+
+				soterPubKeyModel.setCpu_id(jsonObject.getString("cpu_id"));
+				soterPubKeyModel.setUid(jsonObject.getInt("uid"));
+				soterPubKeyModel.setCounter(jsonObject.getLong("counter"));
+			}
+		}catch (Exception e){
+			throw new Exception("Couldn't parse challenge json string in the attestation certificate" + e.getStackTrace());
+		}
 	}
 }
